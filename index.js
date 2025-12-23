41f6a2048f49a721fe09f8ac521a959304beac02
GirishCPatil <girishpatil621@gmail.com>
2025-12-23 11:37:06 +0530

    tables created:   bookings

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'your_password',
-  database: 'testdb'
+  password: 'Girish@21',
+  database: 'bookingsystem_db'
 });
 
 db.connect((err) => {
